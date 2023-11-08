42814b0a5afa2bcbdfc8c42110bd5efa2db6a070
Harsimran Singh Maan <maan.harry@gmail.com>
2023-11-07 20:10:51 -0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func IsPalindrome(i int) bool {
+	// Do your implementation here
+	return false
+}
+
+func main() {
+	fmt.Println(123) // false
+	fmt.Println(121) // true
+	fmt.Println(1)   // true
+	fmt.Println(11)  // true
+	fmt.Println(12)  // false
+}
